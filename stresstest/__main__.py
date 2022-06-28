65c08a55e0425cc07d07a2574746ff8be583922b
Tomas Krejci <tomas@krej.ci>
2022-06-28 15:19:39 +0200

    Results collection

--- stresstest/__main__.py
+++ stresstest/__main__.py
@@ -1,9 +1,17 @@
+import argparse
+
 from stresstest.runner import Runner
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dont-send", help="don't send collected data to server", action="store_true"
+    )
+    args = parser.parse_args()
+
     runner = Runner()
-    runner.run()
+    runner.run(dont_send=args.dont_send)
 
 
 if __name__ == "__main__":
--- stresstest/runner.py
+++ stresstest/runner.py
@@ -1,5 +1,7 @@
 import time
 
+import requests
+from rich.console import Console
 from rich.live import Live
 from rich.pretty import pprint
 from rich.table import Table
@@ -7,14 +9,21 @@ from rich.table import Table
 from stresstest.sensors import collect
 from stresstest.stresstest import BaseStresstest
 
+SERVER_URL = "https://us-central1-iron-envelope-354712.cloudfunctions.net/collect"
+
+
+console = Console()
+
 
 class Runner:
-    def run(self):
+    def run(self, dont_send: bool):
         sensor_data = collect()
 
         table = Table()
         table.add_column("Test name")
         table.add_column("Duration")
+
+        test_results = []
 
         with Live(table, refresh_per_second=4):  # update 4 times a second to feel fluid
             for test in BaseStresstest.discover():
@@ -30,6 +39,35 @@ class Runner:
                     test_duration = test_instance.time()
 
                     table.add_row(f"[green]{test_name}", f"{test_duration:.6f}")
+                    test_results.append(
+                        {
+                            "name": test_name,
+                            "duration": test_duration,
+                        }
+                    )
                 except Exception as e:
                     table.add_row(f"[red]{test_name}", "")
-                    print(e)
+                    test_results.append(
+                        {
+                            "name": test_name,
+                            "duration": "",
+                        }
+                    )
+
+        if not dont_send:
+            console.print(
+                "[red]Collected data will be sent to server [bold]in 3 seconds[/bold] (use [italic]--dont-send[/italic] to avoid this) ..."
+            )
+            time.sleep(3)
+            json_data = {
+                "sensor_data": sensor_data,
+                "test_results": test_results,
+            }
+            response = requests.post(SERVER_URL, json=json_data)
+            if response.status_code == 200:
+                response_json = response.json()
+                collection_id = response_json.get("collection_id")
+                if collection_id:
+                    console.print(
+                        f"Collected data successfully sent to server with collection ID:\n[italic]{collection_id}[/italic]"
+                    )
